1eb763aaa145bc54fda138c0f8f87de0a5dce1d1
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-01-15 14:40:01 +0200

    feat: added app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TasksComponent} from './tasks/tasks.component';
+import {FormTasksComponent} from './tasks/form/form-tasks.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'tasks/:id/edit',
+    component: FormTasksComponent
+  },
+  {
+    path: 'tasks/create',
+    component: FormTasksComponent
+  },
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
